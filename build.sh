02502862ec8effd3ad9d153cb04c0fa25a9e75de
papaL3xa <john.scriven03@gmail.com>
2025-09-17 02:15:45 +0700

    add

--- build.sh
+++ build.sh
@@ -413,14 +413,6 @@ kernelsu ()
 {
     separator
 
-    if ! grep -rnw 'drivers/input/input.c' -e 'CONFIG_KSU' > /dev/null; then
-        quotes "Patching KernelSU to Kernel Tree"
-        separator
-        patch -p1 < <(curl -s "https://raw.githubusercontent.com/papaL3xa/builds/refs/heads/exynos9820/patches/Patching_KernelSU_to_Kernel_Tree.patch")
-        separator
-        check "KernelSU"
-    fi
-
     if ! test -f "arch/arm64/configs/ksu.config"; then
         quotes "Getting KernelSU Next Defconfig"
         curl -LSs "https://raw.githubusercontent.com/papaL3xa/build/refs/heads/exynos9820/configs/$KSU_NEXT" -o arch/arm64/configs/$KSU_NEXT
@@ -435,20 +427,12 @@ kernelsu ()
             rm -rf Ke*
         fi
 
-        git submodule add -b next-susfs-experimental -f -q https://github.com/sidex15/KernelSU-Next > /dev/null
-        bash <(curl -LSs "https://raw.githubusercontent.com/sidex15/KernelSU-Next/refs/heads/next-susfs-experimental/kernel/setup.sh")
+        git submodule add -b next-susfs-experimental https://github.com/sidex15/KernelSU-Next > /dev/null
+        bash <(curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh")
         separator
         check "KernelSU Next"
     fi
 
-    if ! grep -rnw 'fs/Makefile' -e 'CONFIG_KSU_SUSFS' > /dev/null; then
-        separator
-        quotes "Patching SuSFS to Kernel Tree"
-        separator
-        patch -p1 < <(curl -s "https://raw.githubusercontent.com/papaL3xa/builds/refs/heads/exynos9820/patches/implement_SUSFS_v1.5.9.patch")
-        separator
-        check "SuSFS"
-    fi
 }
 
 kernel ()
