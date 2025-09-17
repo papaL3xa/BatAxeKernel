adf74b8e973a843fd8588d17bb9190c22ab688f7
papaL3xa <john.scriven03@gmail.com>
2025-09-17 19:58:37 +0700

    change sub to commit KSUN

--- build.sh
+++ build.sh
@@ -436,7 +436,7 @@ kernelsu ()
             rm -rf Ke*
         fi
 
-        git submodule add -b next-susfs-experimental -f -q https://github.com/sidex15/KernelSU-Next > /dev/null
+        git clone -b next-susfs-experimental -f -q https://github.com/sidex15/KernelSU-Next > /dev/null
         bash <(curl -LSs "https://raw.githubusercontent.com/sidex15/KernelSU-Next/refs/heads/next-susfs-experimental/kernel/setup.sh")
         separator
         check "KernelSU Next"
