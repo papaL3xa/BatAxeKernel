ee7f2672b5a35f986aa72801dd7c5547575c9c45
Signed-off-by: HolyAngel <slverwolf@gmail.com>
2025-09-25 15:59:16 -0400

    Kernel/sched: Reduce Latency [Pafcholini]

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -61,8 +61,8 @@
 unsigned int sysctl_sched_latency			= 24000000ULL;
 static unsigned int normalized_sysctl_sched_latency	= 24000000ULL;
 #else // CONFIG_SCHED_BORE
-unsigned int sysctl_sched_latency			= 6000000ULL;
-unsigned int normalized_sysctl_sched_latency		= 6000000ULL;
+unsigned int sysctl_sched_latency			= 5000000ULL;
+unsigned int normalized_sysctl_sched_latency		= 5000000ULL;
 #endif // CONFIG_SCHED_BORE
 
 /*
