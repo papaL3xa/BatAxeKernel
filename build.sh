f5f4fad53a6cff6df82d46ea6565e1262d55c7d7
papaL3xa <john.scriven03@gmail.com>
2025-11-06 16:00:40 +0700

    update ksun and some on workflow

--- build.sh
+++ build.sh
@@ -448,7 +448,7 @@ kernelsu() {
 
         # Add KernelSU Next submodule
         git submodule add -f -q https://github.com/papaL3xa/KernelSU-Next-gorhanhee.git KernelSU-Next > /dev/null
-        curl -LSs "https://raw.githubusercontent.com/papaL3xa/KernelSU-Next-gorhanhee/refs/heads/next-susfs-experimental/kernel/setup.sh" | bash -
+        curl -LSs "https://raw.githubusercontent.com/papaL3xa/KernelSU-Next-gorhanhee/refs/heads/next-susfs-experiment/kernel/setup.sh" | bash -
         check "KernelSU Next"
     fi
 
